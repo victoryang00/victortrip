b488cf00533aaff84d27361afaa60ab319ea512b
victoryang00 <victoryang00@ucsc.edu>
2022-10-15 13:58:38 -0700

    fix icon

--- gpx_to_json.py
+++ gpx_to_json.py
@@ -4,6 +4,7 @@ import json
 
 input_json = json.load(open('u/victor/2022-Guangdong-Xiangxi'))
 
+
 def get_gpx_data(filename):
     dic = Converter(input_file=filename).gpx_to_dictionary(
         latitude_key='lat', longitude_key='lon')
@@ -12,6 +13,7 @@ def get_gpx_data(filename):
     # print(latitudes)
     longitudes = dic['lon']
     return (latitudes, longitudes)
+
 
 class JsonBuilder:
     def __init__(self, input_json: Dict):
@@ -22,7 +24,8 @@ class JsonBuilder:
         self.json["mapViewData"]["data"][0]["polylines"] = []
         self.json["mapViewData"]["data"][0]["stationIDs"] = []
         self.json["mapViewData"]["data"][0]["stations"] = []
-        self.json["mapViewData"]["data"]= [self.json["mapViewData"]["data"][0]]
+        self.json["mapViewData"]["data"] = [
+            self.json["mapViewData"]["data"][0]]
 
     # @staticmethod
     def add_station(self, index: int, id: str, name: str, lat: float, lng: float, addr: str, postcode: int):
@@ -57,10 +60,27 @@ class JsonBuilder:
             new_poly["points"].append(
                 new_point)
         self.json["mapViewData"]["data"][index]["polylines"].append(new_poly)
-    def change_center(self, lat, lon,distance):
-        self.json["mapViewData"]["center"]["lat"]=lat
-        self.json["mapViewData"]["center"]["lng"]=lon
-        self.json["mapViewData"]["maximumDistance"]=distance
+
+    def add_flight_data(self,index: int, airports: str):
+        new_poly = {"type": "RAIL",
+                     "style": {
+                            "color": 4282339765,
+                            "pattern": "SOLID"
+                        },
+                    "points": []}
+        
+        new_json = {"id": str(id), "name": {"en": name}, "location": {
+            "lat": str(lat), "lng": str(lng), "address": addr, "postcode": str(postcode)}}
+        self.json["mapViewData"]["data"][index]["airports"].append(new_json)
+        self.json["mapViewData"]["data"][index]["airportIDs"].append(id)
+        self.json["mapViewData"]["data"][index]["polylines"].append(new_poly)
+        
+
+    def change_center(self, lat, lon, distance):
+        self.json["mapViewData"]["center"]["lat"] = lat
+        self.json["mapViewData"]["center"]["lng"] = lon
+        self.json["mapViewData"]["maximumDistance"] = distance
+
 
 output = JsonBuilder(input_json)
 
@@ -77,11 +97,11 @@ output.add_station(0, "Los Angeles", "Los Angeles, CA, USA",
 output.add_station(0, "Irvine", "Irvine, CA, USA",
                    33.6839, -117.7947, "Irvine, CA, USA", 92618)
 
-output.add_car_data(0,*get_gpx_data("gpx/From_SC_to_LA.gpx"))
-output.add_train_data(0,*get_gpx_data("gpx/LA_to_IV.gpx"))  
-output.add_car_data(0,*get_gpx_data("gpx/Morning_Ride.gpx"))  
-output.add_car_data(0,*get_gpx_data("gpx/josef.gpx"))  
-output.change_center(34.4208305 ,-119.6981901,600)  
-out_file = open("u/victor/2022-SantaCruz-LA-Irvine", "w")
+output.add_car_data(0, *get_gpx_data("gpx/From_SC_to_LA.gpx"))
+output.add_train_data(0, *get_gpx_data("gpx/LA_to_IV.gpx"))
+output.add_car_data(0, *get_gpx_data("gpx/Morning_Ride.gpx"))
+output.add_car_data(0, *get_gpx_data("gpx/josef.gpx"))
+output.change_center(34.4208305, -119.6981901, 600)
+out_file=open("u/victor/2020-Japan-Korea", "w")
 
 json.dump(output.json, out_file, indent=4)
